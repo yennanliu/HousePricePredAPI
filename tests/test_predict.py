3b8a8c532e651daa049fc8d25edf1b1a7f2e7958
yennanliu <f339339@gmail.com>
2020-01-23 23:29:49 +0800

    add unittest : test_predict, test_predict_with_input,  test_predict_with_nonvalidated_input

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -54,5 +54,22 @@ def test_train():
     result = h._train()
     assert str(type(result)) == "<class 'pandas.core.frame.DataFrame'>"
 
+def test_predict():
+    h = HousePricePredictor()
+    result = h._predict()
+    assert type(result) == list
+
+def test_predict_with_input():
+    input_json = {"MSSubClass":20.0,"LotFrontage":100.0,"LotArea":17500.0,"OverallQual":7.0,"OverallCond":8.0,"YearBuilt":1959.0,"YearRemodAdd":2002.0,"MasVnrArea":0.0,"BsmtFinSF1":1406.0,"BsmtFinSF2":0.0,"BsmtUnfSF":496.0,"TotalBsmtSF":1902.0,"1stFlrSF":1902.0,"2ndFlrSF":0.0,"LowQualFinSF":0.0,"GrLivArea":1902.0,"BsmtFullBath":1.0,"BsmtHalfBath":0.0,"FullBath":2.0,"HalfBath":0.0,"BedroomAbvGr":3.0,"KitchenAbvGr":1.0,"TotRmsAbvGrd":7.0,"Fireplaces":2.0,"GarageYrBlt":1959.0,"GarageCars":2.0,"GarageArea":567.0,"WoodDeckSF":0.0,"OpenPorchSF":207.0,"EnclosedPorch":162.0,"3SsnPorch":0.0,"ScreenPorch":0.0,"PoolArea":0.0,"MiscVal":0.0,"MoSold":5.0,"YrSold":2010.0}
+    h = HousePricePredictor()
+    result = h._predict_with_input(input_json)
+    assert type(result) == str
+
+def test_predict_with_nonvalidated_input():
+    input_json = {"myjson":123}
+    h = HousePricePredictor()
+    result = h._predict_with_input(input_json)
+    assert result == None
+
 if __name__ == '__main__':
     pytest.main([__file__])
