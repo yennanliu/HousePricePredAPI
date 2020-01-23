2801392870f7cf522b2d742bda6288e882e46a82
yennanliu <f339339@gmail.com>
2020-01-23 22:49:38 +0800

    add unittest : test_load_model, test_process_data

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -23,5 +23,16 @@ def test_save_model():
     result = h._save_model(model)
     assert result == True
 
+def test_load_model():
+    h = HousePricePredictor()
+    model = h._load_model()
+    assert str(type(model)) == "<class 'sklearn.linear_model._base.LinearRegression'>"
+
+def test_process_data():
+    h = HousePricePredictor()
+    df_train, df_test = h._process_data() 
+    assert str(type(df_train)) == "<class 'pandas.core.frame.DataFrame'>"
+    assert str(type(df_test)) == "<class 'pandas.core.frame.DataFrame'>"
+
 if __name__ == '__main__':
     pytest.main([__file__])
